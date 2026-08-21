ec505a8abb43ad623e03203bd125a3b289891b7d
tanveerahmed9413 <tanveerahmed4872@gmail.com>
2026-08-21 21:38:36 +0530

    change url

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://auratune-vqd1.onrender.com",
         credentials: true,
     })
 );
